bd7892ad81dda6d5d053aac9e9a49c5991f98255
Andy Chapman <achapman@degdigital.com>
2019-09-30 16:48:17 -0400

    Fix page rendering for wordpress pages/posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const slug = require('slug')
+const slash = require(`slash`)
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
@@ -20,24 +21,86 @@ exports.createPages = async ({ graphql, actions }) => {
 
   return graphql(`
     {
-      allStripeSku {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            product {
-              id
-              name
-            }
-          }
-        }
-      }
+		allWordpressPage {
+			edges {
+				node {
+					id
+					path
+					status
+					template
+				}
+			}
+		}
+		allWordpressPost {
+			edges {
+				node {
+					id
+					path
+					status
+					template
+					format
+				}
+			}
+		}
+    	allStripeSku {
+        	edges {
+          		node {
+            		fields {
+              			slug
+            		}
+            		product {
+              			id
+              			name
+            		}
+          		}
+        	}
+      	}
     }
   `).then(result => {
     if (result.errors) {
       Promise.reject(result.errors)
-    }
+	}
+
+	// Access query results via object destructuring
+	const { allWordpressPage, allWordpressPost } = result.data
+
+	// Create Page pages.
+	const pageTemplate = path.resolve(`./src/templates/page.js`)
+	// We want to create a detailed page for each page node.
+	// The path field contains the relative original WordPress link
+	// and we use it for the slug to preserve url structure.
+	// The Page ID is prefixed with 'PAGE_'
+	allWordpressPage.edges.forEach(edge => {
+	  // Gatsby uses Redux to manage its internal state.
+	  // Plugins and sites can use functions like "createPage"
+	  // to interact with Gatsby.
+	  createPage({
+		// Each page is required to have a `path` as well
+		// as a template component. The `context` is
+		// optional but is often necessary so the template
+		// can query data specific to each page.
+		path: edge.node.path,
+		component: slash(pageTemplate),
+		context: {
+		  id: edge.node.id,
+		},
+	  })
+	})
+  
+	const postTemplate = path.resolve(`./src/templates/post.js`)
+	// We want to create a detailed page for each post node.
+	// The path field stems from the original WordPress link
+	// and we use it for the slug to preserve url structure.
+	// The Post ID is prefixed with 'POST_'
+	allWordpressPost.edges.forEach(edge => {
+	  createPage({
+		path: edge.node.path,
+		component: slash(postTemplate),
+		context: {
+		  id: edge.node.id,
+		},
+	  })
+	})
 
     // Create product pages
     const products = {}
@@ -46,7 +109,7 @@ exports.createPages = async ({ graphql, actions }) => {
       products[node.product.id] = node.fields.slug
     })
 
-    const productTemplate = path.resolve('src/templates/ProductTemplate.js')
+    const productTemplate = path.resolve('src/templates/product.js')
     Object.entries(products).forEach(([id, slug]) => {
       createPage({
         path: 'buy/' + slug,
