e7abf243be67c8665f0a1bd3f05c41ef706f4421
Andy Chapman <achapman@degdigital.com>
2019-10-03 17:29:36 -0400

    Footer Began, Sponsor Bar Completed

---
+++ src/components/sponsorBar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+import {
+	Container, Row, Col
+} from 'reactstrap'
+
+import stylesSponsorBar from '../styles/components/sponsorbar.module.scss'
+
+import ruggersLogo from '../images/sponsors/ruggers.png'
+import duganLogo from '../images/sponsors/dugan.png'
+import essmcLogo from '../images/sponsors/essmc.png'
+
+const SponsorBar = () => {
+	return (
+		<div className={stylesSponsorBar.sponsorBar}>
+			<Container>
+				<Row>
+					<Col className={stylesSponsorBar.sponsor} xs="12" md="4">
+						<a href="/dugan-associates-supports-pittsburghs-rugby-club" className="dugan">
+							<img src={duganLogo} alt="Dugan &amp; Associates" class={stylesSponsorBar.sponsorLogo}/>
+						</a>
+					</Col>
+					<Col className={stylesSponsorBar.sponsor} xs="12" md="4">
+						<a href="/pittsburgh-forge-partners-with-essmc" className="essmc">
+							<img src={essmcLogo} alt="Eastern Suburbs Sports Medicine" class={stylesSponsorBar.sponsorLogo}/>
+						</a>
+					</Col>
+					<Col className={stylesSponsorBar.sponsor} xs="12" md="4">
+						<a href="http://ruggerspub.com/" className="ruggers" target="_blank">
+							<img src={ruggersLogo} alt="Ruggers Pub" class={stylesSponsorBar.sponsorLogo}/>
+						</a>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	)
+}
+
+export default SponsorBar
