29ee2b566db048942727b94f5a3bb371962cdd6d
Andy Chapman <achapman@degdigital.com>
2019-10-10 14:38:06 -0400

    Install Slick & Update product page image calls

--- src/components/productPage.js
+++ src/components/productPage.js
@@ -1,36 +1,72 @@
 import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import {
+	Container, Row, Col, Breadcrumb, BreadcrumbItem, Button
+} from 'reactstrap';
+import Slider from "react-slick";
 import Img from 'gatsby-image'
 import { ProductsContext } from './productsProvider'
 import { CartContext } from './cartProvider'
 
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 const ProductPage = ({ productId }) => {
   const { products } = useContext(ProductsContext)
+  const { skus } = useContext(ProductsContext)
   const { add, toggle } = useContext(CartContext)
 
   const product = products[productId]
+  const variations = product.skus
 
   return (
-    <div style={{ margin: '0 auto', maxWidth: 500 }}>
-      <div style={{ margin: '3rem auto', maxWidth: 300 }}>
-        {product.localFiles && (
-          <Img fluid={product.localFiles[0].childImageSharp.fluid} />
-        )}
-      </div>
-      <h2>{product.name}</h2>
-      <div>{product.caption}</div>
-      <br />
-      <div style={{ textAlign: 'justify' }}>{product.description}</div>
-      <button
-        style={{ margin: '2rem auto' }}
-        onClick={() => {
-          add(product.skus[0].id)
-          toggle(true)
-        }}
-      >
-        Add To Cart
-      </button>
-    </div>
+	<Container>
+		<Row className="content">
+			<Col xs="12">
+				<div className="content-main">
+					<Row>
+						<Col xs="12" md="6" className="pdpImages">
+							<Slider className="pdpImgSlider">
+								{product.skus[0].product.localFiles && (
+									<Img fluid={product.skus[0].product.localFiles[0].childImageSharp.fluid} />
+								)}
+								{variations.map(variation => {
+									return (
+										<Img fluid={variation.localFiles[0].childImageSharp.fluid} />
+									)
+								})}
+							</Slider>
+						</Col>
+						<Col xs="12" md="6" className="pdpDetails">
+							<h1>{product.name}</h1>
+							<p>{product.id}</p>
+							{variations.map(variation => {
+
+								return (
+									<React.Fragment>
+										<p>{variation.id} - {variation.attributes.name}</p>
+										<p>${variation.price / 100}</p>
+									</React.Fragment>
+								)
+							})}
+							<div>{product.caption}</div>
+							<br />
+							<div style={{ textAlign: 'justify' }}>{product.description}</div>
+							<button
+								style={{ margin: '2rem auto' }}
+								onClick={() => {
+								add(product.skus[0].id)
+								toggle(true)
+								}}
+							>
+								Add To Cart
+							</button>
+						</Col>
+					</Row>
+				</div>
+			</Col>
+		</Row>
+	</Container>
   )
 }
 
--- src/templates/product.js
+++ src/templates/product.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Layout from "../layouts/layout"
+import SEO from '../components/SEO';
 import ProductPage from '../components/productPage'
 
 const ItemTemplate = ({ pageContext: { id } }) => {
   return (
     <Layout>
-      <ProductPage productId={id} />
+    	<ProductPage productId={id} />
     </Layout>
   )
 }
