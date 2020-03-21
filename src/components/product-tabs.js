e3cfefe4e8a531c156160eee76e69fd2b40ba13b
Andy Chapman <achapman@degdigital.com>
2020-03-21 13:30:32 -0400

    Cleanup

--- src/components/product-tabs.js
+++ src/components/product-tabs.js
@@ -1,46 +1,63 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTabList = styled('ul')`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    list-style: none;
+    margin: 40px 0 0 0;
+    padding: 0 10px;
+    list-style-type: none;
+    border-bottom: 1px solid #333;
+`;
+
+const StyledTabListItem = styled('li')`
+    &:not(:last-of-type) {
+        margin-right: 15px;
+    }
+`;
+
+const StyledTabButton = styled('button')`
+    display: inline-block;
+    font-size: 18px;
+    line-height: 1;
+    padding: 10px 15px;
+    background: white;
+    border: 1px solid #333;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+    margin-bottom: -1px;
+    cursor: pointer;
+    &[aria-selected='true'] {
+        border-bottom-color: white;
+    }
+`;
+const StyledTabPanel = styled('div')`
+    padding: 0 10px;
+`;
 
 const ProductTabs = ({ tabs }) => {
 
-    const [activeTab, setActiveTab] = useState('1');
-
-    const toggle = tab => {
-        if(activeTab !== tab) setActiveTab(tab);
-    }
-
+    const [activeTabIndex, setActiveTabIndex] = React.useState(0);
     if (!tabs.length) {
         return null;
     }
 
     return (
-        <div>
-            <Nav tabs>
+        <>
+            <StyledTabList role="tablist">
                 {tabs.map((tab, index) => (
-                    <NavItem key={tab.id} role="presentation">
-                        <NavLink
-                        className={classnames({ active: activeTab === index })}
-                        onClick={() => { toggle(index); }}
-                        >
+                    <StyledTabListItem key={tab.id} role="presentation">
+                        <StyledTabButton role="tab" aria-selected={tabs[activeTabIndex].id === tab.id} onClick={() => setActiveTabIndex(index)}>
                             {tab.title}
-                        </NavLink>
-                    </NavItem>
+                        </StyledTabButton>
+                    </StyledTabListItem>
                 ))}
-            </Nav>
-            <TabContent activeTab={activeTab}>
-                {tabs.map((tab, index) => (
-                    <TabPane tabId={index}>
-                        <Row>
-                            <Col sm="12"
-                                dangerouslySetInnerHTML={{ __html: tab.content }}
-                            />
-                        </Row>
-                    </TabPane>
-                ))}
-            </TabContent>
-        </div>
+            </StyledTabList>
+            <StyledTabPanel role="tabpanel" aria-label={tabs[activeTabIndex].title} dangerouslySetInnerHTML={{ __html: tabs[activeTabIndex].content }} />
+        </>
     );
 };
 
