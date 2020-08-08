737d1092418d88e4e381e13bcfb8d7ad8e44197a
Andy Chapman <achapman@degdigital.com>
2020-08-08 15:20:33 -0400

    Remove Product References

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,14 +3,14 @@ import { Link } from 'gatsby'
 import Layout from "../layouts/layout"
 import SEO from '../components/SEO'
 import ProductPagination from '../components/product-pagination';
-import ProductsList from '../components/products-list';
+// import ProductsList from '../components/products-list';
 
 const ProductsTemplate = ({ products }) => {
     return (
         <Layout>
             <SEO title="Products" />
             <Link to="/">Go back to the homepage</Link>
-            <ProductsList products={products} />
+            {/* <ProductsList products={products} /> */}
             {/* <ProductPagination pageCount={pageContext.pageCount} currentPage={pageContext.currentPage} /> */}
         </Layout>
     );
