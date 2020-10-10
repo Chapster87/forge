ce1ab62eacf9adccae4cee688b8b19863b099e01
Andy Chapman <achapman@degdigital.com>
2020-10-10 17:08:01 -0400

    Cherry pick sitemap from Bg Vid Branch

---
+++ src/pages/sitemap.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react"
+import { Link, graphql } from "gatsby"
+import { FaRegClock } from "react-icons/fa"
+
+import Layout from "../layouts/layout"
+import SEO from '../components/SEO'
+
+class SiteMap extends Component {
+  render() {
+    const data = this.props.data
+
+    return (
+      <Layout>
+        <React.Fragment>
+		  <SEO title="Site Map" keywords={[`gatsby`, `application`, `react`]} />
+		  <Link to="/products">Go to the products - NOT WORKING</Link>
+          <h1>Pages</h1>
+          {data.allWordpressPage.edges.map(({ node }) => (
+            <div key={node.slug}>
+              <Link to={node.slug} css={{ textDecoration: `none` }}>
+                <h3>{node.title}</h3>
+              </Link>
+              <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+              <span>
+                <FaRegClock
+                  size={14}
+                  css={{ position: `relative`, bottom: 1 }}
+                />
+                {` `}
+                {node.date}
+              </span>
+            </div>
+          ))}
+        </React.Fragment>
+        <hr />
+        <h1>Posts</h1>
+        {data.allWordpressPost.edges.map(({ node }) => (
+          <div key={node.slug}>
+            <Link to={node.slug} css={{ textDecoration: `none` }}>
+              <h3>{node.title}</h3>
+            </Link>
+            <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </div>
+        ))}
+      </Layout>
+    )
+  }
+}
+
+export default SiteMap
+
+// Set here the ID of the home page.
+export const pageQuery = graphql`
+  query {
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+          date(formatString: "MMMM DD, YYYY")
+        }
+      }
+    }
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
